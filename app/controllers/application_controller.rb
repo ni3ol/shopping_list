aaed332fa246ea1d144194a74bc03ba73304f7d9
Nicol <nvojacek@gmail.com>
2016-11-12 12:41:31 +0200

    Init repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def shopping_list
+    render html: 'This is going to be epic!'
+  end
+end
